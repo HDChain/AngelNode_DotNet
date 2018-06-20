0976a99bec268915e7edc408d99f4a8ce3dcd21f
jon <jon.zhao@hdchain.io>
2018-06-20 16:42:40 +0800

    [added]:ipfs depend
    1.

--- PartnerNode/PartnerNode/Controllers/DefaultController.cs
+++ PartnerNode/PartnerNode/Controllers/DefaultController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using Ipfs.Api;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Nethereum.JsonRpc.Client;
@@ -13,6 +17,7 @@ namespace PartnerNode.Controllers
     [Route("api/Default")]
     public class DefaultController : Controller
     {
+        [Route("Get")]
         [HttpGet]
         public string Get(string account) {
             var client = new RpcClient(new Uri("http://ethereum:8545"));
@@ -22,6 +27,18 @@ namespace PartnerNode.Controllers
             var resp = web3.Eth.GetBalance.SendRequestAsync(account).Result;
 
             return resp.HexValue;
+        }
+
+        [Route("GetFile")]
+        [HttpGet]
+        public async Task<HttpResponseMessage> GetFile(string cid) {
+            var client = new IpfsClient("http://ipfs:5001");
+
+            var node = await client.DownloadBytesAsync("cat", default(CancellationToken), cid);
+
+            return new HttpResponseMessage(HttpStatusCode.OK) {
+                Content = new ByteArrayContent(node)
+            };
         }
     }
 }
